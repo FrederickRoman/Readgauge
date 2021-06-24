a62aab37a296a71517e47aaeee279e5324d09984
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2021-06-23 21:39:14 -0700

    feat(syllableCountService): For short input word lists, predict sequentially with memoization; for long ones do it in batches. Tidy tensor memory for both.

--- src/services/syllableCount/SyllableCountService.ts
+++ src/services/syllableCount/SyllableCountService.ts
@@ -1,4 +1,14 @@
-import { LayersModel, tensor2d, Tensor } from "@tensorflow/tfjs";
+import {
+  LayersModel,
+  tensor,
+  tensor1d,
+  tensor2d,
+  Tensor,
+  Tensor2D,
+  tidy,
+} from "@tensorflow/tfjs";
+
+import memoize from "fast-memoize";
 
 function wordEncode(input: string): number[] {
   const wordLowerCase = input.toLowerCase();
@@ -42,6 +52,72 @@ function indexOfMax(arr: number[]): number {
   return indexOfMax;
 }
 
+function countSyllsSeq(words: string[], model: LayersModel): number {
+  console.time("count 2");
+  const MAX_NUM_OF_CHARS: number = 33;
+  const LENGTH_OF_CHAR_ENCODING: number = 5;
+  const INPUT_LENGTH: number = MAX_NUM_OF_CHARS * LENGTH_OF_CHAR_ENCODING;
+  const INPUT_SIZE: [number, number] = [1, INPUT_LENGTH];
+  const predictSyllableCount = (word: string): number =>
+    tidy((): number => {
+      const encodedWord: number[] = wordEncode(word);
+      const inputTensor: Tensor2D = tensor2d(encodedWord, INPUT_SIZE);
+      const outputTensor = model.predict(inputTensor) as Tensor;
+      const outputArray = outputTensor.arraySync() as number[][];
+      const syllableCount: number = indexOfMax(outputArray[0]);
+      return syllableCount;
+    });
+  const memoPredictSyllabeCount: (word: string) => number =
+    memoize(predictSyllableCount);
+
+  let totSyllableCount: number = 0;
+  words.forEach((word: string): void => {
+    totSyllableCount += memoPredictSyllabeCount(word);
+  });
+  console.timeEnd("count 2");
+  return totSyllableCount;
+}
+
+// function countSyllsParallel(words: string[], model: LayersModel): number {
+//   console.time("count 1");
+//   // const encodedWord: number[] = wordEncode(word);
+//   // const inputTensor = tf.tensor2d(encodedWord, [1, encodedWord.length]);
+//   // const outputTensor = model.predict(inputTensor) as tf.Tensor;
+//   // const outputArray = outputTensor.arraySync() as number[][];
+//   // const syllableCount: number = indexOfMax(outputArray[0]);
+
+//   const encodedWords: number[][] = words.map((word) => wordEncode(word));
+//   const numOfInputs = encodedWords.length;
+//   const lenghtOfInputs = 33 * 5;
+//   const inputSize: [number, number] = [encodedWords.length, 33 * 5];
+//   const inputTensor = tensor2d(encodedWords, inputSize);
+//   const outputTensor = model.predict(inputTensor) as Tensor;
+//   const outputArray = outputTensor.arraySync() as number[][];
+//   const syllableCount: number[] = outputArray.map((t) => indexOfMax(t));
+//   const count = syllableCount.reduce((acc, cur) => acc + cur);
+//   console.timeEnd("count 1");
+//   return count;
+// }
+
+function countSyllsParallel(words: string[], model: LayersModel): number {
+  console.time("count 1");
+  const encodedWords: number[][] = words.map((word) => wordEncode(word));
+  const numOfInputs = encodedWords.length;
+  const MAX_NUM_OF_CHARS: number = 33;
+  const LENGTH_OF_CHAR_ENCODING: number = 5;
+  const INPUT_LENGTH: number = MAX_NUM_OF_CHARS * LENGTH_OF_CHAR_ENCODING;
+  const INPUT_SIZE: [number, number] = [numOfInputs, INPUT_LENGTH];
+  const totSyllableCount: number = tidy((): number => {
+    const inputTensor: Tensor2D = tensor2d(encodedWords, INPUT_SIZE);
+    const outputTensor = model.predict(inputTensor) as Tensor;
+    const outputArray = outputTensor.arraySync() as number[][];
+    const syllableCount: number[] = outputArray.map((t) => indexOfMax(t));
+    return syllableCount.reduce((acc, cur) => acc + cur);
+  });
+  console.timeEnd("count 1");
+  return totSyllableCount;
+}
+
 class SyllableCountService {
   static countSyllables(word: string): number {
     // // const MODEL_DIR = "file://./neuralNet/model.json";
@@ -59,22 +135,58 @@ class SyllableCountService {
     return word.length;
   }
   static countSyllablesM(words: string[], model: LayersModel): number {
-    // const encodedWord: number[] = wordEncode(word);
-    // const inputTensor = tf.tensor2d(encodedWord, [1, encodedWord.length]);
-    // const outputTensor = model.predict(inputTensor) as tf.Tensor;
-    // const outputArray = outputTensor.arraySync() as number[][];
-    // const syllableCount: number = indexOfMax(outputArray[0]);
-
-    const encodedWords: number[][] = words.map((word) => wordEncode(word));
-    const numOfInputs = encodedWords.length;
-    const lenghtOfInputs = 33 * 5;
-    const inputSize: [number, number] = [encodedWords.length, 33 * 5];
-    const inputTensor = tensor2d(encodedWords, inputSize);
-    const outputTensor = model.predict(inputTensor) as Tensor;
-    const outputArray = outputTensor.arraySync() as number[][];
-    const syllableCount: number[] = outputArray.map((t) => indexOfMax(t));
-    return syllableCount.reduce((acc, cur) => acc + cur);
+    const WORD_NUM_THRESHOLD = 1000;
+    const numOfWords = words.length;
+    return numOfWords < WORD_NUM_THRESHOLD
+      ? countSyllsSeq(words, model)
+      : countSyllsParallel(words, model);
   }
+  // static countSyllablesM(words: string[], model: LayersModel): number {
+  //   const lenghtOfInput = 33 * 5;
+  //   const inputSize: [number, number] = [1, lenghtOfInput];
+  //   const getSyll = (encodedWord: number[]) =>
+  //     tidy(() => {
+  //       const inputTensor = tensor2d(encodedWord, inputSize);
+  //       const outputTensor = model.predict(inputTensor) as Tensor;
+  //       const outputArray = outputTensor.arraySync() as number[][];
+  //       const syllableCount = indexOfMax(outputArray[0]);
+  //       return syllableCount;
+  //     });
+  //   const memoGetSyll = memoize(getSyll);
+
+  //   const encodedWords: number[][] = words.map((word) => wordEncode(word));
+  //   let tot = 0;
+  //   encodedWords.forEach((encodedWord) => {
+  //     const syllableCount: number = memoGetSyll(encodedWord);
+  //     tot += syllableCount;
+  //   });
+  //   return tot;
+  // }
+  // static countSyllablesM(words: string[], model: LayersModel): number {
+  //   console.time("count 2");
+  //   const MAX_NUM_OF_CHARS: number = 33;
+  //   const LENGTH_OF_CHAR_ENCODING: number = 5;
+  //   const INPUT_LENGTH: number = MAX_NUM_OF_CHARS * LENGTH_OF_CHAR_ENCODING;
+  //   const INPUT_SIZE: [number, number] = [1, INPUT_LENGTH];
+  //   const predictSyllableCount = (word: string): number =>
+  //     tidy(() => {
+  //       const encodedWord: number[] = wordEncode(word);
+  //       const inputTensor: Tensor2D = tensor2d(encodedWord, INPUT_SIZE);
+  //       const outputTensor = model.predict(inputTensor) as Tensor;
+  //       const outputArray = outputTensor.arraySync() as number[][];
+  //       const syllableCount: number = indexOfMax(outputArray[0]);
+  //       return syllableCount;
+  //     });
+  //   const memoPredictSyllabeCount: (word: string) => number =
+  //     memoize(predictSyllableCount);
+
+  //   let tot: number = 0;
+  //   words.forEach((word: string): void => {
+  //     tot += memoPredictSyllabeCount(word);
+  //   });
+  //   console.timeEnd("count 2");
+  //   return tot;
+  // }
 }
 
 export default SyllableCountService;
