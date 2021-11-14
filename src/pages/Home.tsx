db077c3d734333973b80abcf242ddafb47fef264
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2021-11-14 13:46:00 -0800

    refactor(Home): Remove unused code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,45 +1,14 @@
-// import useReadabilityReducer from "../hooks/useReadabilityReducer";
-
+import useScore from "../hooks/useScore";
 import RSinputSection from "../components/readScale/input/RSinputSection";
 import RSoutputSection from "../components/readScale/output/RSoutputSection";
 
-import { useState } from "react";
-import { useEffect } from "react";
-
-import * as tf from "@tensorflow/tfjs";
-import useScore from "../hooks/useScore";
-
 function Home(): JSX.Element {
-  // const [readability, readabilityDispatch] = useReadabilityReducer();
-  // const [text, setText] = useState<string>("");
-  // const [score, setScore] = useState<number>(0);
-  // const [model, setModel] = useState<tf.LayersModel | null>(null);
-
-  const { text, setText, score, setScore, running } = useScore();
-
-  // useEffect(() => {
-  //   async function loadModel() {
-  //     const ROOT_DIR = "http://localhost:3000";
-  //     const MODEL_PUBLIC_DIR: string = `${ROOT_DIR}/neuralNet/model.json`;
-  //     const model: tf.LayersModel = await tf.loadLayersModel(MODEL_PUBLIC_DIR);
-  //     setModel(model);
-  //   }
-  //   loadModel();
-  // }, []);
-
-  // useEffect(() => {
-  //   console.log("text");
-  //   console.log(text);
-  // }, [text]);
-
+  const { text, setText, score, running } = useScore();
   return (
     <>
-      <RSinputSection
-        text={text}
-        setText={setText}
-      />
+      <RSinputSection text={text} setText={setText} />
       <RSoutputSection score={score} />
-      <div> {running? 'running':'idle'}</div>
+      <div> {running ? "running" : "idle"}</div>
     </>
   );
 }
