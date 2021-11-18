ebd0002484d865a06820c79f1cc83d9ca9f178d8
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2021-11-17 20:21:09 -0800

    feat(App): Wrap app with Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
+import { BrowserRouter as Router } from "react-router-dom";
 import MainBar from "./components/navigation/MainBar";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
+    <Router>
       <MainBar />
       <Home />
-    </>
+    </Router>
   );
 }
 
