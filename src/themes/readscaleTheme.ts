26f99ad5c90e23303ac0ae6485c378110a557899
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2021-11-17 19:56:05 -0800

    feat(readscaleTheme): Create custom theme's palette

--- src/themes/readscaleTheme.ts
+++ src/themes/readscaleTheme.ts
@@ -1,5 +1,18 @@
 import { createTheme, Theme } from "@mui/material/styles";
+import { grey, orange, deepPurple } from "@mui/material/colors";
 
-const readscaleTheme: Theme = createTheme();
+const readscaleTheme: Theme = createTheme({
+  palette: {
+    primary: {
+      main: deepPurple[800],
+    },
+    secondary: {
+      main: orange[200],
+    },
+    background: {
+      default: grey[50],
+    },
+  },
+});
 
 export default readscaleTheme;
