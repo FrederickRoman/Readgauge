f2542428ebb1dc908b9606a87f32d1d0b34c8e0f
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2021-11-18 13:46:49 -0800

    refactor(BannerTextSection): Break into subcomponents

--- src/components/banner/BannerTextSection.tsx
+++ src/components/banner/BannerTextSection.tsx
@@ -3,6 +3,28 @@ import logo from "../../assets/img/logo.svg";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 
+function BrandLogoImg(): JSX.Element {
+  return <img src={logo} alt="readgauge logo" width="60" height="60" />;
+}
+
+function BrandLogoName(): JSX.Element {
+  return <span style={{ color: "white", fontSize: "2em" }}>Readgauge</span>;
+}
+
+function BrandSlogan(): JSX.Element {
+  return (
+    <h2
+      style={{
+        color: "white",
+        textAlign: "center",
+        verticalAlign: "middle",
+      }}
+    >
+      AI-powered reading level scoring app
+    </h2>
+  );
+}
+
 function BannerTextSection(): JSX.Element {
   return (
     <Grid container justifyContent="center" alignItems="center">
@@ -10,28 +32,19 @@ function BannerTextSection(): JSX.Element {
         <Box p={1}>
           <Grid container justifyContent="center" alignItems="center">
             <Grid item>
-              <img src={logo} alt="readgauge logo" width="60" height="60" />
+              <BrandLogoImg />
             </Grid>
             <Grid item>
-              <span style={{ color: "white" , fontSize:"2em"}}>Readgauge</span>
+              <BrandLogoName />
             </Grid>
           </Grid>
         </Box>
       </Grid>
-
       <Grid item>
         <Box px={1}>
           <Grid container justifyContent="center" alignItems="center">
             <Grid item>
-              <h2
-                style={{
-                  color: "white",
-                  textAlign: "center",
-                  verticalAlign: "middle",
-                }}
-              >
-                AI-powered reading level scoring app
-              </h2>
+              <BrandSlogan />
             </Grid>
           </Grid>
         </Box>
