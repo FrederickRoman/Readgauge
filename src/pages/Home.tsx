02a1ac17efc3b8a3637983b19f602d3ec1fc30d6
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2021-12-07 01:04:20 -0800

    feat(Home): Wrap page content with main tags.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,15 +2,13 @@ import HeroBanner from "../components/banner/HeroBanner";
 import IntroDemoSection from "../components/content/home/IntroDemoSection";
 import ReadscaleDemo from "../components/readScale/ReadscaleDemo";
 
-
 function Home(): JSX.Element {
   return (
-    <>
+    <main>
       <HeroBanner />
       <IntroDemoSection />
- 
       <ReadscaleDemo />
-    </>
+    </main>
   );
 }
 
