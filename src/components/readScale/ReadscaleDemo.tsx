a28e37e8a4daf7fa9e68e0e56366a90a3c34f9af
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2021-12-14 00:44:08 -0800

    feat(ReadscaleDemo): Modify responsive layout.

--- src/components/readScale/ReadscaleDemo.tsx
+++ src/components/readScale/ReadscaleDemo.tsx
@@ -50,40 +50,60 @@ function ReadscaleDemo(): JSX.Element {
   };
 
   return (
-    <Box component="section" mb={10} sx={{bgcolor:"background.paper"}}>
+    <Box
+      component="section"
+      mx={1}
+      mb={10}
+      sx={{ bgcolor: "background.paper" }}
+    >
       <BookCurveDivider place="top" />
-      <Container maxWidth="xl" >
-        <Grid container >
-          <Grid item xs={12} sm={6}  sx={{bgcolor:"background.paper"}}>
-            <Box
-              sx={{
-                flexGrow: 1,
-                bgcolor:"background.paper !important",
-                display: "flex",
-                height: 224,
-              }}
-            >
-              <Tabs
-                orientation="vertical"
-                variant="scrollable"
-                value={value}
-                onChange={handleChange}
-                aria-label="Vertical tabs example"
-                sx={{ borderRight: 1, borderColor: "divider" , bgcolor:"background.paper"}}
-              >
-                <Tab label="Text" {...a11yProps(0)} />
-                <Tab label="File" {...a11yProps(1)} />
-              </Tabs>
-              <TabPanel value={value} index={0}>
-                <RSinputSection text={text} setText={setText} />
-              </TabPanel>
-              <TabPanel value={value} index={1}>
-              <Box my={5}>
-                <RSfileInputSection setText={setText} /> </Box>
-              </TabPanel>
-            </Box>
+      <Container maxWidth="xl">
+        <Grid
+          container
+          justifyContent="center"
+          alignItems="center"
+          sx={{ bgcolor: "background.paper" }}
+        >
+          <Grid item xs={12} sm={6}>
+            <Grid container justifyContent="center" alignItems="center">
+              <Grid item>
+                {" "}
+                <Box
+                  sx={{
+                    flexGrow: 1,
+                    bgcolor: "background.paper !important",
+                    display: "flex",
+                    height: 224,
+                  }}
+                >
+                  <Tabs
+                    orientation="vertical"
+                    variant="scrollable"
+                    value={value}
+                    onChange={handleChange}
+                    aria-label="Vertical tabs example"
+                    sx={{
+                      borderRight: 1,
+                      borderColor: "divider",
+                      bgcolor: "background.paper",
+                    }}
+                  >
+                    <Tab label="Text" {...a11yProps(0)} />
+                    <Tab label="File" {...a11yProps(1)} />
+                  </Tabs>
+                  <TabPanel value={value} index={0}>
+                    <RSinputSection text={text} setText={setText} />
+                  </TabPanel>
+                  <TabPanel value={value} index={1}>
+                    <Box my={5}>
+                      <RSfileInputSection setText={setText} />{" "}
+                    </Box>
+                  </TabPanel>
+                </Box>
+              </Grid>
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={6} sx={{bgcolor:"background.paper"}}>
+          <Grid item xs={12} sm={6} sx={{ bgcolor: "background.paper" }}>
             <Box my={5}>
               <RSoutputSection score={score} running={running} />
             </Box>
