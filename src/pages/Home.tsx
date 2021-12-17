d80bae7f421fdf76ce5ec718742d3e90323edc0f
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2021-12-16 16:35:58 -0800

    feat(Home): Set the column layout explicitly

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,22 @@
 import HeroBanner from "../components/banner/HeroBanner";
 import IntroDemoSection from "../components/content/home/IntroDemoSection";
 import ReadscaleDemo from "../components/readScale/ReadscaleDemo";
+import { Grid } from "@mui/material";
 
 function Home(): JSX.Element {
   return (
     <main>
-      <HeroBanner />
-      <IntroDemoSection />
-      <ReadscaleDemo />
+      <Grid container flexDirection="column">
+        <Grid item>
+          <HeroBanner />
+        </Grid>
+        <Grid item>
+          <IntroDemoSection />
+        </Grid>
+        <Grid item>
+          <ReadscaleDemo />
+        </Grid>
+      </Grid>
     </main>
   );
 }
