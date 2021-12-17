b228e7152b5ae22b8a6d2d10d8fbeaf5d8d86b65
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2021-12-16 16:28:28 -0800

    test(App): Test the App renders with text 'ReadGauge' in at least one element.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App with text ReadGauge', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const readgaugeTextElement = screen.getAllByText(/ReadGauge/i);
+  expect(readgaugeTextElement[0]).toBeInTheDocument();
 });
