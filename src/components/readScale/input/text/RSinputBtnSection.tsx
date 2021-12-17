c5ccadffd195844061d4850954d4b1ea946b9f84
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2021-12-16 16:15:43 -0800

    feat(RSinputBtnSection): Add margin to IconButton.

--- src/components/readScale/input/text/RSinputBtnSection.tsx
+++ src/components/readScale/input/text/RSinputBtnSection.tsx
@@ -1,19 +1,27 @@
+import { Box } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
 
-interface IProps {
+interface Props {
   setText: React.Dispatch<React.SetStateAction<string>>;
 }
 
-function RSinputBtnSection(props: IProps): JSX.Element {
+function RSinputBtnSection(props: Props): JSX.Element {
   const { setText } = props;
 
   const handleClick = (): void => setText("");
 
   return (
-    <IconButton name="clear" aria-label="delete" onClick={handleClick} size="large">
-      <DeleteIcon />
-    </IconButton>
+    <Box m={1}>
+      <IconButton
+        name="clear"
+        aria-label="delete"
+        onClick={handleClick}
+        size="large"
+      >
+        <DeleteIcon />
+      </IconButton>
+    </Box>
   );
 }
 
