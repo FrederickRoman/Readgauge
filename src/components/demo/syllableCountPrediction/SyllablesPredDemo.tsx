1a46a9a09c09008ee8988fd27a2548f965150f4f
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2021-12-19 15:11:58 -0800

    feat(SyllablesPredDemo): Add syllable prediction demo that displays on a chart the syllable count prediction probability for an input word.

--- src/components/demo/syllableCountPrediction/SyllablesPredDemo.tsx
+++ src/components/demo/syllableCountPrediction/SyllablesPredDemo.tsx
@@ -8,20 +8,12 @@ import {
   tidy,
 } from "@tensorflow/tfjs";
 import Preprocessing from "../../../services/syllableCount/Preprocessing";
+import SyllablesPredChart from "./chart/SyllablesPredChart";
 
-function SyllablesPredDemo(): JSX.Element {
+function useSyllableCountDemo() {
   const [text, setText] = useState<string>("");
   const [model, setModel] = useState<LayersModel | null>(null);
-  const [probs, setProbs] = useState<number[][]>([]);
-
-  const handleSubmit = (event: FormEvent<HTMLFormElement>): void => {
-    event.preventDefault();
-  };
-
-  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
-    const changedText = event.target.value;
-    setText(changedText);
-  };
+  const [probs, setProbs] = useState<number[]>([]);
 
   useEffect(() => {
     async function loadModel(): Promise<void> {
@@ -34,46 +26,76 @@ function SyllablesPredDemo(): JSX.Element {
   }, []);
 
   useEffect(() => {
-    function predictWordSyllableCount(word: string): number[][] {
+    function getWordFromText(text: string): string {
+      const isLetter = (char: string): boolean => /^[a-zA-Z]+$/.test(char);
+      const word = `${text}`.split("").filter(isLetter).join("");
+      return word;
+    }
+    function predictWordSyllableCount(word: string): number[] {
       const MAX_NUM_OF_CHARS: number = 33;
       const LENGTH_OF_CHAR_ENCODING: number = 5;
       const INPUT_LENGTH: number = MAX_NUM_OF_CHARS * LENGTH_OF_CHAR_ENCODING;
       const INPUT_SIZE: [number, number] = [1, INPUT_LENGTH];
-      if (model) {
-        return tidy(() => {
-          const encodedWord: number[] = Preprocessing.memoWordEncoder(word);
-          const inputTensor: Tensor2D = tensor2d(encodedWord, INPUT_SIZE);
-          const outputTensor = model.predict(inputTensor) as Tensor2D;
-          const outputArray = outputTensor.arraySync() as number[][];
-          return outputArray;
-        });
-      } else return [];
+      if (word) {
+        if (model) {
+          return tidy(() => {
+            const encodedWord: number[] = Preprocessing.memoWordEncoder(word);
+            const inputTensor: Tensor2D = tensor2d(encodedWord, INPUT_SIZE);
+            const outputTensor = model.predict(inputTensor) as Tensor2D;
+            const outputArray = outputTensor.arraySync() as number[][];
+            return outputArray.flat();
+          });
+        } else return Array(15).fill(1 / 15);
+      } else return [1, ...Array(14).fill(0)];
     }
-    const probs: number[][] = predictWordSyllableCount(text);
+    const word: string = getWordFromText(text);
+    const probs: number[] = predictWordSyllableCount(word);
     setProbs(probs);
   }, [text, model]);
+
+  return { setText, probs };
+}
+
+function SyllablesPredDemo(): JSX.Element {
+  const [value, setValue] = useState<string>("");
+  const { setText, probs } = useSyllableCountDemo();
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    const changedText = event.target.value;
+    setValue(changedText);
+    setText(changedText);
+  };
 
   return (
     <Grid container flexDirection="column">
       <Grid item>
-        <Box
-          component="form"
-          noValidate
-          autoComplete="off"
-          onSubmit={handleSubmit}
-        >
-          <TextField
-            id="outlined-multiline-static"
-            label="Write word here"
-            variant="outlined"
-            type="search"
-            value={text}
-            onChange={handleChange}
-          />
-        </Box>
+        <Grid container justifyContent="center" alignItems="center">
+          <Grid item>
+            <Box
+              component="form"
+              noValidate
+              autoComplete="off"
+              onSubmit={handleSubmit}
+            >
+              <TextField
+                id="syllable-pred-demo_text-field"
+                label="Write word here"
+                variant="outlined"
+                type="search"
+                helperText="one word only (e.g. cat)"
+                value={value}
+                onChange={handleChange}
+              />
+            </Box>
+          </Grid>
+        </Grid>
       </Grid>
       <Grid item>
-        <pre>{JSON.stringify(probs, null, " ")}</pre>
+        <SyllablesPredChart predProbs={probs} />
       </Grid>
     </Grid>
   );
---
+++ src/components/demo/syllableCountPrediction/chart/SyllablesPredChart.tsx
@@ -0,0 +1,85 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import { deepPurple, orange } from "@mui/material/colors";
+import { Box } from "@mui/material";
+
+interface Props {
+  predProbs: number[];
+}
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const chartOptions = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top",
+      display: false,
+    },
+  },
+  scales: {
+    x: {
+      title: {
+        color: deepPurple[800],
+        display: true,
+        text: "Number of syllables",
+      },
+      ticks: {
+        font: {
+          size: 20,
+        },
+      },
+    },
+    y: {
+      title: {
+        color: deepPurple[800],
+        display: true,
+        text: "Probability %",
+      },
+      ticks: {
+        font: {
+          size: 20,
+        },
+      },
+    },
+  },
+} as const;
+
+function SyllablesPredChart(props: Props): JSX.Element {
+  const { predProbs } = props;
+  const chartData = {
+    labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+    datasets: [
+      {
+        label: "Predictions",
+        data: predProbs.map((prob) => 100 * prob),
+        borderColor: deepPurple[800],
+        backgroundColor: orange[200],
+      },
+    ],
+  };
+  return (
+    <Box width="80vw">
+      <Line options={chartOptions} data={chartData} />
+    </Box>
+  );
+}
+
+export default SyllablesPredChart;
