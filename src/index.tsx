cce020cd82023e5d2e9d1265962bf1595ba0a5da
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-02-15 21:16:41 -0800

    refactor(src): Remove unused css.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import { ErrorBoundary } from "react-error-boundary";
 import CssBaseline from "@mui/material/CssBaseline";
