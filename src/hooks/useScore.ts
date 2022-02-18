50f742e5d68048b4f2cc186998f865a69619bc77
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-02-17 21:33:02 -0800

    refactor(useScore): Remove console.time

--- src/hooks/useScore.ts
+++ src/hooks/useScore.ts
@@ -1,5 +1,8 @@
+/**
+ * This custom hook is the reactive bridge between the UI components 
+ * and the scoring services pipeline.
+ */
 import { useState, useEffect } from "react";
-
 import { loadLayersModel, LayersModel } from "@tensorflow/tfjs";
 
 import TokenizationService from "../services/tokenization/TokenizationService";
@@ -8,18 +11,26 @@ import FleshReadingEaseService from "../services/readability/scales/fleschReadin
 
 import ItextFeatures from "../types/interfaces/ItextFeatures";
 
+type model = LayersModel | null;
+interface Scoring {
+  text: string;
+  setText: React.Dispatch<React.SetStateAction<string>>;
+  score: number;
+  setScore: React.Dispatch<React.SetStateAction<number>>;
+  running: boolean;
+}
+
 const { tokenizeToWords, tokenizeToSentences } = TokenizationService;
 const { countSyllables } = SyllableCountService;
 const { fkGradeLevel } = FleshReadingEaseService;
 
-type model = LayersModel | null;
-
-function useScore() {
+function useScore(): Scoring {
   const [text, setText] = useState<string>("");
   const [score, setScore] = useState<number>(0);
   const [model, setModel] = useState<model>(null);
   const [running, setRunning] = useState<boolean>(false);
 
+  /* load/unload the neural network model */
   useEffect(() => {
     async function loadModel(): Promise<void> {
       const MODEL_PUBLIC_DIR = `${process.env.PUBLIC_URL}/neuralNet/model.json`;
@@ -30,38 +41,28 @@ function useScore() {
     return () => setModel(null);
   }, []);
 
+  /* recompute the score on text change, and toggle loader */
   useEffect(() => {
     async function computeScore(model: model, text: string): Promise<void> {
       try {
         if (model && text) {
           setRunning(true);
-          // console.time("computing score");
-          // console.time("tokenization");
           const tokenizing: Promise<string[]>[] = [
             tokenizeToSentences(text),
             tokenizeToWords(text),
           ];
           const [sentences, words]: string[][] = await Promise.all(tokenizing);
-          // console.timeEnd("tokenization");
-
           const countPredInput = { words, model };
           const totNumWords: number = words.length;
           const totNumSentences: number = sentences.length;
-          // console.time("syllable counting");
           const totNumSyllables: number = await countSyllables(countPredInput);
-          // console.timeEnd("syllable counting");
-
           const textFeatures: ItextFeatures = Object.freeze({
             totNumWords,
             totNumSentences,
             totNumSyllables,
           });
-
-          // console.time("formula application");
           const fkGradeLevelScore: number = fkGradeLevel(textFeatures);
-          // console.timeEnd("formula application");
           setScore(fkGradeLevelScore);
-          // console.timeEnd("computing score");
           setRunning(false);
         } else {
           setScore(Infinity);
@@ -72,15 +73,9 @@ function useScore() {
         setRunning(false);
       }
     }
-
     const timeoutId = setTimeout(() => computeScore(model, text), 300);
     return () => clearTimeout(timeoutId);
   }, [text, model]);
-
-  // useEffect(() => {
-  //   console.log("score---");
-  //   console.log(score);
-  // }, [score]);
 
   return { text, setText, score, setScore, running };
 }
